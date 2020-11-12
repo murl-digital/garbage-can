c1fc22f6587a86283fb5914603203dea7a3afaa0
Joe Sorensen <joseph.md.sorensen@icloud.com>
2020-11-11 21:23:54 -0700

    added basic discord bot stuff

---
+++ GarbageCan/GarbageCan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace GarbageCan
+{
+    class GarbageCan
+    {
+        private static DiscordClient _client;
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            _client = new DiscordClient(new DiscordConfiguration
+            {
+                Token = null, //implement this later
+                TokenType = TokenType.Bot
+            });
+            
+            //... fun init logic to be done later
+
+            await _client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
