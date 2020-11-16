851d1e7b291df5a7c5e5994b89cb9190d679876c
Joe Sorensen <joseph.md.sorensen@icloud.com>
2020-11-15 22:13:37 -0700

    handled application termination

--- GarbageCan/GarbageCan.cs
+++ GarbageCan/GarbageCan.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Config.Net;
 using DSharpPlus;
+using DSharpPlus.Entities;
 
 namespace GarbageCan
 {
@@ -11,6 +15,36 @@ namespace GarbageCan
 	{
 		private static DiscordClient _client;
 		public static IBotConfig config;
+		
+		#region Trap application termination
+		[DllImport("Kernel32")]
+		private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+		private delegate bool EventHandler(CtrlType sig);
+		static EventHandler _handler;
+
+		enum CtrlType {
+			CTRL_C_EVENT = 0,
+			CTRL_BREAK_EVENT = 1,
+			CTRL_CLOSE_EVENT = 2,
+			CTRL_LOGOFF_EVENT = 5,
+			CTRL_SHUTDOWN_EVENT = 6
+		}
+
+		private static bool Handler(CtrlType sig) {
+			Console.WriteLine("Exiting system due to external CTRL-C, or process kill, or shutdown");
+			
+			_client.UpdateStatusAsync(null, UserStatus.Offline).GetAwaiter().GetResult();
+			_client.Dispose();
+
+			Console.WriteLine("Cleanup complete");
+
+			//shutdown right away so there are no lingering threads
+			Environment.Exit(-1);
+
+			return true;
+		}
+		#endregion
 
 		private static void Main(string[] args)
 		{
@@ -37,6 +71,11 @@ namespace GarbageCan
 				IFeature feature = (IFeature) Activator.CreateInstance(t);
 				feature.init(_client);
 			}
+
+			_client.Ready += (eventArgs) => _client.UpdateStatusAsync(new DiscordGame("dang"));
+			
+			_handler += Handler;
+			SetConsoleCtrlHandler(_handler, true);
 
 			await _client.ConnectAsync();
 			await Task.Delay(-1);
