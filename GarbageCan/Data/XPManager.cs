2ad5df611c4d76f4fe5dc4914e0ed4059c318010
Joe Sorensen <joseph.md.sorensen@icloud.com>
2020-11-15 17:52:12 -0700

    added first implementation of XPManager

---
+++ GarbageCan/Data/XPManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarbageCan.Data
+{
+    public class XPManager
+    {
+        public static async Task<XPUser> RetrieveUser(string id)
+        {
+            await using var context = new XPContext();
+            return await context.xp_users.SingleOrDefaultAsync(u => u.id.Equals(id));
+        }
+
+        public static void SaveUser(string id, int lvl, double xp)
+        {
+            using var context = new XPContext();
+            var entity = context.xp_users.Find(id);
+
+            if (entity == null)
+            {
+                entity = new XPUser();
+                context.xp_users.Add(entity);
+            }
+
+            entity.id = id;
+            entity.lvl = lvl;
+            entity.xp = xp;
+
+            context.SaveChangesAsync();
+        }
+
+        public static void RemoveUser(string id)
+        {
+            using var context = new XPContext();
+            var entity = context.xp_users.Find(id);
+            if (entity == null) return;
+            
+            context.xp_users.Remove(entity);
+            context.SaveChangesAsync();
+        }
+    }
+}
