6e79c57c4af00b0fd4182598fb37a869272fdc78
Joe Sorensen <joseph.md.sorensen@gmail.com>
2020-12-02 08:03:49 -0700

    imagine missing the unversioned files

---
+++ GarbageCan/XP/Data/XpContext.cs
@@ -0,0 +1,23 @@
+﻿using GarbageCan.XP.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarbageCan.XP.Data
+{
+	public class XpContext : DbContext
+	{
+		private string _connectionString;
+		public DbSet<EntityUser> xpUsers { get; set; }
+		public DbSet<EntityQueuedBooster> xpQueuedBoosters { get; set; }
+		public DbSet<EntityAvailableSlot> xpAvailableSlots { get; set; }
+		public DbSet<EntityActiveBooster> xpActiveBoosters { get; set; }
+		public DbSet<EntityUserBooster> xpUserBoosters { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (GarbageCan.Config == null) GarbageCan.BuildConfig();
+			_connectionString =
+				$"host={GarbageCan.Config.address};port={GarbageCan.Config.port};user id={GarbageCan.Config.user};password={GarbageCan.Config.password};database={GarbageCan.Config.xpSchema}";
+			optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+		}
+	}
+}
