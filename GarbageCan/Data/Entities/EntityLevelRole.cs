1bde16dd5d115a0c9bb0c0a3de3ea37bb9650204
Joe Sorensen <joseph.md.sorensen@gmail.com>
2020-12-29 11:43:34 -0700

    added first iteration of xp level roles

---
+++ GarbageCan/Data/Entities/EntityLevelRole.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarbageCan.Data.Entities
+{
+    [Table("xp_level_roles")]
+    [Keyless]
+    public class EntityLevelRole
+    {
+        public int lvl { get; set; }
+        public ulong id { get; set; }
+        public bool remain { get; set; }
+    }
+}
---
+++ GarbageCan/Roles/RoleManager.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using GarbageCan.Data;
+using GarbageCan.XP;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarbageCan.Roles
+{
+    public class RoleManager : IFeature
+    {
+        public void Init(DiscordClient client)
+        {
+            XpManager.GhostLevelUp += PlaceholderName;
+        }
+
+        public void Cleanup()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public static void PlaceholderName(object sender, XpEventArgs args)
+        {
+            Task.Run(() =>
+            {
+                var lvlArgs = (LevelUpArgs) args;
+                using var context = new Context();
+                context.xpLevelRoles.Where(r => r.lvl == lvlArgs.oldLvl).ForEachAsync(r =>
+                {
+                    if (r.remain) return;
+                    var role = args.context.Guild.GetRole(r.id);
+                    args.context.Guild.GetMemberAsync(args.id).ContinueWith(t => t.Result.RevokeRoleAsync(role));
+                });
+                context.xpLevelRoles.Where(r => r.lvl == lvlArgs.lvl).ForEachAsync(r =>
+                {
+                    var role = args.context.Guild.GetRole(r.id);
+                    args.context.Guild.GetMemberAsync(args.id).ContinueWith(t => t.Result.GrantRoleAsync(role));
+                });
+            });
+        }
+    }
+}
