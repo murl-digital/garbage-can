6b69b78b5131b5156e803f9bd1d4a80e46903928
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-01-07 11:32:36 -0700

    refactored main class to be a little less dumb

--- GarbageCan/GarbageCan.cs
+++ GarbageCan/GarbageCan.cs
@@ -21,66 +21,62 @@ namespace GarbageCan
 		public static IBotConfig Config;
 		
 		private static List<IFeature> _botFeatures;
-		private static readonly AutoResetEvent Closing = new AutoResetEvent(false);
 
 		private static void Main(string[] args)
 		{
-			MainAsync().ConfigureAwait(false).GetAwaiter().GetResult();
-		}
-
-		private static async Task MainAsync()
-		{
-			Log.Logger = new LoggerConfiguration()
-				.WriteTo.Console()
-				.CreateLogger();
-
-			var logFactory = new LoggerFactory().AddSerilog();
-
-			Log.Information("Reading config...");
-			BuildConfig();
-
-			Client = new DiscordClient(new DiscordConfiguration
+			Task.Run(async () =>
 			{
-				Token = Config.token,
-				TokenType = TokenType.Bot,
-				LoggerFactory = logFactory,
-				MinimumLogLevel = LogLevel.Debug
+				Log.Logger = new LoggerConfiguration()
+					.WriteTo.Console()
+					.CreateLogger();
+
+				var logFactory = new LoggerFactory().AddSerilog();
+
+				Log.Information("Reading config...");
+				BuildConfig();
+
+				Client = new DiscordClient(new DiscordConfiguration
+				{
+					Token = Config.token,
+					TokenType = TokenType.Bot,
+					LoggerFactory = logFactory,
+					MinimumLogLevel = LogLevel.Debug
+				});
+
+				Log.Information("Initializing features...");
+				_botFeatures = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
+					.Where(x => typeof(IFeature).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+					.Select(x => (IFeature) Activator.CreateInstance(x))
+					.ToList();
+
+				foreach (var feature in _botFeatures)
+				{
+					Log.Information("Feature " + feature.GetType().Name + " found, attempting to initialize...");
+					feature.Init(Client);
+					Log.Information("Success!");
+				}
+
+				Commands = Client.UseCommandsNext(new CommandsNextConfiguration
+				{
+					StringPrefixes = new[] {Config.commandPrefix}
+				});
+
+				Commands.RegisterCommands(Assembly.GetExecutingAssembly());
+
+				Client.Ready += (_, _) =>
+				{
+					Log.Information("ready");
+					return Task.CompletedTask;
+				};
+
+				await Client.ConnectAsync();
+
+				AppDomain.CurrentDomain.ProcessExit += Shutdown;
+				AppDomain.CurrentDomain.DomainUnload += Shutdown;
+				Console.CancelKeyPress += Shutdown;
 			});
-			
-			Log.Information("Initializing features...");
-			_botFeatures = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
-				.Where(x => typeof(IFeature).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
-				.Select(x => (IFeature)Activator.CreateInstance(x))
-				.ToList();
 
-			foreach (var feature in _botFeatures)
-			{
-				Log.Information("Feature " + feature.GetType().Name + " found, attempting to initialize...");
-				feature.Init(Client);
-				Log.Information("Success!");
-			}
-
-			Commands = Client.UseCommandsNext(new CommandsNextConfiguration
-			{
-				StringPrefixes = new[] { Config.commandPrefix }
-			});
-
-			Commands.RegisterCommands(Assembly.GetExecutingAssembly());
-
-			_handler += Shutdown;
-			
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-			{
-				SetConsoleCtrlHandler(_handler, true);
-			}
-			else
-			{
-				Console.CancelKeyPress += (_, _) => Shutdown(CtrlType.CTRL_C_EVENT);
-				AppDomain.CurrentDomain.ProcessExit += (sender, args) => Shutdown(CtrlType.CTRL_C_EVENT);
-			}
-
-			await Client.ConnectAsync();
-			Closing.WaitOne();
+			while (!Environment.HasShutdownStarted) {}
 		}
 
 		public static void BuildConfig()
@@ -93,7 +89,7 @@ namespace GarbageCan
 			if (Config == null) throw new NullReferenceException("Attempted to build config, but got null");
 		}
 		
-		private static bool Shutdown(CtrlType sig)
+		private static void Shutdown(object? sender, EventArgs eventArgs)
 		{
 			Log.Information("Shutting down...");
 
@@ -103,30 +99,6 @@ namespace GarbageCan
 			}
 			Client.UpdateStatusAsync(null, UserStatus.Offline).GetAwaiter().GetResult();
 			Client.Dispose();
-
-			Closing.Set();
-
-			return true;
 		}
-
-		#region Windows specific shutdown stuff
-
-		[DllImport("Kernel32")]
-		private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
-
-		private delegate bool EventHandler(CtrlType sig);
-
-		private static EventHandler _handler;
-
-		private enum CtrlType
-		{
-			CTRL_C_EVENT = 0,
-			CTRL_BREAK_EVENT = 1,
-			CTRL_CLOSE_EVENT = 2,
-			CTRL_LOGOFF_EVENT = 5,
-			CTRL_SHUTDOWN_EVENT = 6
-		}
-		
-		#endregion
 	}
 }
