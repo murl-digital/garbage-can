c1cc4e02a61a3504e0431c9f44676c78c227f03a
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-02-26 13:43:34 -0700

    prepped for web api development

---
+++ GarbageCan/Web/Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using GarbageCan.Data;
+using GarbageCan.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarbageCan.Web.Controllers
+{
+    [ApiController]
+    [Route("status")]
+    public class StatusController
+    {
+        [HttpGet]
+        public async Task<Status> Get()
+        {
+            var result = new Status
+            {
+                ping = GarbageCan.Client.Ping,
+            };
+
+            return result;
+        }
+    }
+}
---
+++ GarbageCan/Web/Models/Status.cs
@@ -0,0 +1,7 @@
+namespace GarbageCan.Web.Models
+{
+    public class Status
+    {
+        public int ping { get; init; }
+    }
+}
