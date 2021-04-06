8a8e3f535126a484caf8831655a98c90e11d21e4
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-04-06 12:17:31 -0600

    added rate limiting just in case

--- GarbageCan/Web/Startup.cs
+++ GarbageCan/Web/Startup.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
+using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
@@ -16,6 +19,7 @@ namespace GarbageCan.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
             services.AddSwaggerGen(c =>
             {
                 c.EnableAnnotations();
@@ -32,6 +36,7 @@ namespace GarbageCan.Web
                     }
                 });
             });
+            
             services.AddCors(options =>
             {
                 options.AddPolicy("gbc",
@@ -52,11 +57,34 @@ namespace GarbageCan.Web
                     NameClaimType = ClaimTypes.NameIdentifier
                 };
             });
+
+            services.AddMemoryCache();
+            services.Configure<IpRateLimitOptions>(options =>
+            {
+                options.EnableEndpointRateLimiting = true;
+                options.StackBlockedRequests = true;
+                options.GeneralRules = new List<RateLimitRule>
+                {
+                    new()
+                    {
+                        Endpoint = "*",
+                        Period = "30s",
+                        Limit = 20
+                    }
+                };
+            });
+            services.Configure<IpRateLimitPolicies>(_ => {});
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseIpRateLimiting();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
