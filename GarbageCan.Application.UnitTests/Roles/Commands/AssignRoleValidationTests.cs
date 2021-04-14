2b3d0e65c047d106519081c09de0379fd7ff8dea
Tim Grannen <grannendev@gmail.com>
2021-04-14 00:11:38 -0400

    Add Assign Role command Validator and unit tests

---
+++ GarbageCan.Application.UnitTests/Roles/Commands/AssignRoleValidationTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using GarbageCan.Application.Common.Exceptions;
+using GarbageCan.Application.Reactions.Commands.AssignRole;
+using GarbageCan.Application.UnitTests.Shared;
+using GarbageCan.Domain.Entities;
+using NUnit.Framework;
+
+namespace GarbageCan.Application.UnitTests.Roles.Commands
+{
+    public class AssignRoleValidationTests
+    {
+        private readonly ApplicationFixture _fixture;
+
+        public AssignRoleValidationTests()
+        {
+            _fixture = new ApplicationFixture();
+        }
+
+        [Theory]
+        public void ShouldThrowValidationException_WhenEmojiIsNull()
+        {
+            var command = new AssignRoleCommand
+            {
+                GuildId = 151,
+                UserId = 450,
+                ChannelId = 4540,
+                MessageId = 500,
+                Emoji = null,
+            };
+
+            FluentActions.Invoking(() => _fixture.SendAsync(command)).Should().Throw<ValidationException>();
+        }
+
+        [Theory]
+        [TestCase(0u, 41u, 41u, 41u, 41u, "TEST")]
+        [TestCase(41u, 0u, 41u, 41u, 41u, "TEST")]
+        [TestCase(41u, 41u, 0u, 41u, 41u, "TEST")]
+        [TestCase(41u, 41u, 41u, 0u, 41u, "TEST")]
+        [TestCase(41u, 41u, 41u, 41u, 0u, null)]
+        [TestCase(41u, 41u, 41u, 41u, 0u, "")]
+        [TestCase(41u, 41u, 41u, 41u, 0u, "   ")]
+        public void ShouldThrowValidationException_WhenHasInvalidProperty(ulong guildId, ulong userId, ulong channelId, ulong messageId, ulong emojiId, string emojiName)
+        {
+            var command = new AssignRoleCommand
+            {
+                GuildId = guildId,
+                UserId = userId,
+                ChannelId = channelId,
+                MessageId = messageId,
+                Emoji = new Emoji
+                {
+                    Id = emojiId,
+                    Name = emojiName,
+                },
+            };
+
+            FluentActions.Invoking(() => _fixture.SendAsync(command)).Should().Throw<ValidationException>();
+        }
+    }
+}
---
+++ GarbageCan.Application/Reactions/Commands/AssignRole/AssignRoleCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace GarbageCan.Application.Reactions.Commands.AssignRole
+{
+    public class AssignRoleCommandValidator : AbstractValidator<AssignRoleCommand>
+    {
+        public AssignRoleCommandValidator()
+        {
+            RuleFor(v => v.UserId).GreaterThan(0u);
+            RuleFor(v => v.ChannelId).GreaterThan(0u);
+            RuleFor(v => v.GuildId).GreaterThan(0u);
+            RuleFor(v => v.MessageId).GreaterThan(0u);
+            RuleFor(v => v.Emoji).NotNull();
+
+            When(x => x.Emoji != null, () =>
+            {
+                RuleFor(x => x.Emoji.Name).NotNull().NotEmpty().When(x => x.Emoji.Id == 0);
+            });
+        }
+    }
+}
