0d79c79a24af764808fb5af49534fd786244817c
Tim Grannen <grannendev@gmail.com>
2021-04-18 11:08:44 -0400

    Add Excluded Channel entity a keyless entity

--- GarbageCan.Domain/Entities/XP/EntityExcludedChannel.cs
+++ GarbageCan.Domain/Entities/XP/EntityExcludedChannel.cs
@@ -1,6 +1,5 @@
 namespace GarbageCan.Domain.Entities.XP
 {
-    [Keyless]
     public class EntityExcludedChannel
     {
         public ulong channelId { get; set; }
---
+++ GarbageCan.Infrastructure/Persistence/Configurations/EntityExcludedChannelConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GarbageCan.Domain.Entities.XP;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GarbageCan.Infrastructure.Persistence.Configurations
+{
+    public class EntityExcludedChannelConfiguration : IEntityTypeConfiguration<EntityExcludedChannel>
+    {
+        public void Configure(EntityTypeBuilder<EntityExcludedChannel> builder)
+        {
+            builder.HasNoKey();
+        }
+    }
+}
