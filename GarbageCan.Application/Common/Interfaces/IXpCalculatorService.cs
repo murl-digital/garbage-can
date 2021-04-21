3fc37b9f9c2e1776bfe40b4ba00a0f4903cc2845
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-04-20 19:37:07 -0600

    added first version of xp handling

---
+++ GarbageCan.Application/Common/Interfaces/IXpCalculatorService.cs
@@ -0,0 +1,16 @@
+﻿namespace GarbageCan.Application.Common.Interfaces
+{
+    public interface IXpCalculatorService
+    {
+        double XpEarned(string message);
+        double XpRequired(int level);
+
+        double TotalXpRequired(int lvl)
+        {
+            var result = 0.0;
+            for (var i = 0; i <= lvl; i++) result += XpRequired(i);
+
+            return result;
+        }
+    }
+}
