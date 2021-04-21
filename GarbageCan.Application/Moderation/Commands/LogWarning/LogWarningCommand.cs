acab8174ecadc5f38715b52832aca8fc47ac4f8a
Tim Grannen <grannendev@gmail.com>
2021-04-20 21:38:22 -0400

    Add Log Warning command and module

---
+++ GarbageCan.Application/Moderation/Commands/LogWarning/LogWarningCommand.cs
@@ -0,0 +1,49 @@
+﻿using GarbageCan.Application.Common.Interfaces;
+using GarbageCan.Domain.Entities.Moderation;
+using GarbageCan.Domain.Enums;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GarbageCan.Application.Moderation.Commands.LogWarning
+{
+    public class LogWarningCommand : IRequest<bool>
+    {
+        public ulong UserId { get; set; }
+        public string Comments { get; set; }
+    }
+
+    public class LogWarningCommandHandler : IRequestHandler<LogWarningCommand, bool>
+    {
+        private readonly IDiscordResponseService _responseService;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IApplicationDbContext _context;
+        private readonly IDateTime _dateTime;
+
+        public LogWarningCommandHandler(IDiscordResponseService responseService, ICurrentUserService currentUserService, IApplicationDbContext context, IDateTime dateTime)
+        {
+            _responseService = responseService;
+            _currentUserService = currentUserService;
+            _context = context;
+            _dateTime = dateTime;
+        }
+
+        public async Task<bool> Handle(LogWarningCommand request, CancellationToken cancellationToken)
+        {
+            var now = _dateTime.Now.ToUniversalTime();
+            var tryParse = ulong.TryParse(_currentUserService.UserId, out ulong test);
+            var log = new ActionLog
+            {
+                uId = request.UserId,
+                mId = tryParse ? test : 0,
+                issuedDate = now,
+                punishmentLevel = PunishmentLevel.VerbalWarning,
+                comments = request.Comments
+            };
+            await _context.ModerationActionLogs.AddAsync(log, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            await _responseService.RespondAsync($"Verbal warning logged with id {log.id}", true);
+            return true;
+        }
+    }
+}
---
+++ GarbageCan.Web/Commands/Moderation/ModerationCommandModule.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using GarbageCan.Application.Moderation.Commands.LogWarning;
+using System.Threading.Tasks;
+
+namespace GarbageCan.Web.Commands.Moderation
+{
+    public class ModerationCommandModule : MediatorCommandModule
+    {
+        [Command("logWarning")]
+        [Aliases("lw")]
+        [RequireRoles(RoleCheckMode.All, "Staff")]
+        public async Task LogWarning(CommandContext ctx, DiscordMember member, [RemainingText] string comments)
+        {
+            await Mediator.Send(new LogWarningCommand { Comments = comments, UserId = member.Id }, ctx);
+        }
+    }
+}
