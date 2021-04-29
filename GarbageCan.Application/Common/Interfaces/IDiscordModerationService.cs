9ca8105eb6b21e3e3187615ff4490470d6a31292
Tim Grannen <grannendev@gmail.com>
2021-04-29 18:15:39 -0400

    Add Discord Moderation Service to be able to ban a user

---
+++ GarbageCan.Application/Common/Interfaces/IDiscordModerationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace GarbageCan.Application.Common.Interfaces
+{
+    public interface IDiscordModerationService
+    {
+        Task BanAsync(ulong guildId, ulong userId, string reason = null);
+    }
+}
---
+++ GarbageCan.Infrastructure/Discord/DiscordModerationService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using GarbageCan.Application.Common.Interfaces;
+
+namespace GarbageCan.Infrastructure.Discord
+{
+    public class DiscordModerationService : IDiscordModerationService
+    {
+        private readonly DiscordClient _client;
+
+        public DiscordModerationService(DiscordClient client)
+        {
+            _client = client;
+        }
+
+        public async Task BanAsync(ulong guildId, ulong userId, string reason = null)
+        {
+            var member = await GetRole(guildId, userId);
+            await member.BanAsync(reason: reason);
+        }
+
+        private async Task<DiscordMember> GetRole(ulong guildId, ulong userId)
+        {
+            var guild = await _client.GetGuildAsync(guildId);
+            var member = await guild.GetMemberAsync(userId);
+            return member;
+        }
+    }
+}
