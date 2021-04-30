13bc0187c2566f848a2dedecc05f6d9dc5e750d0
Tim Grannen <grannendev@gmail.com>
2021-04-30 00:33:26 -0400

    Ignore messages created that start with the command prefix for XP calculation

--- GarbageCan.Application/XP/EventHandlers/DiscordMessageCreatedEventHandler.cs
+++ GarbageCan.Application/XP/EventHandlers/DiscordMessageCreatedEventHandler.cs
@@ -1,23 +1,31 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
+﻿using GarbageCan.Application.Common.Interfaces;
 using GarbageCan.Application.Common.Models;
 using GarbageCan.Application.XP.Commands.AddXpToUser;
 using GarbageCan.Domain.Events;
 using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GarbageCan.Application.XP.EventHandlers
 {
     public class DiscordMessageCreatedEventHandler : INotificationHandler<DomainEventNotification<DiscordMessageCreatedEvent>>
     {
         private readonly IMediator _mediator;
+        private readonly IDiscordConfiguration _configuration;
 
-        public DiscordMessageCreatedEventHandler(IMediator mediator)
+        public DiscordMessageCreatedEventHandler(IMediator mediator, IDiscordConfiguration configuration)
         {
             _mediator = mediator;
+            _configuration = configuration;
         }
 
         public async Task Handle(DomainEventNotification<DiscordMessageCreatedEvent> notification, CancellationToken cancellationToken)
         {
+            if (notification.DomainEvent.Content.StartsWith(_configuration.CommandPrefix))
+            {
+                return;
+            }
+
             await _mediator.Send(new AddXpToUserCommand
             {
                 UserId = notification.DomainEvent.AuthorId,
