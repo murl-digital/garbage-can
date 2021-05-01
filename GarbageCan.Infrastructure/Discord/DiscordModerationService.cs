ed463e7389221192017ccad70342aba3511524e7
Tim Grannen <grannendev@gmail.com>
2021-04-30 20:19:42 -0400

    Add Channel restore access to moderation service

--- GarbageCan.Infrastructure/Discord/DiscordModerationService.cs
+++ GarbageCan.Infrastructure/Discord/DiscordModerationService.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Entities;
 using GarbageCan.Application.Common.Interfaces;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace GarbageCan.Infrastructure.Discord
@@ -25,6 +26,14 @@ namespace GarbageCan.Infrastructure.Discord
             var member = await GetRole(guildId, userId);
             var channel = await _client.GetChannelAsync(channelId);
             await channel.AddOverwriteAsync(member, Permissions.None, Permissions.AccessChannels);
+        }
+
+        public async Task RestoreChannelAccess(ulong guildId, ulong userId, ulong channelId, string reason = null)
+        {
+            var member = await GetRole(guildId, userId);
+            var channel = await _client.GetChannelAsync(channelId);
+
+            await channel.PermissionOverwrites.First(o => o.Id == member.Id).DeleteAsync("channel restrict expired");
         }
 
         private async Task<DiscordMember> GetRole(ulong guildId, ulong userId)
