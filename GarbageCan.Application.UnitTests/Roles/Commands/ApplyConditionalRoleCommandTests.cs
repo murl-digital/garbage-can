d3b71531d35fd6d9c7366b771301c7d0d9f98ddb
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-06-01 23:05:33 -0600

    added applyconditionalrolescommand and associated tests

---
+++ GarbageCan.Application.UnitTests/Roles/Commands/ApplyConditionalRoleCommandTests.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using GarbageCan.Application.Common.Interfaces;
+using GarbageCan.Application.UnitTests.Shared;
+using GarbageCan.Domain.Entities.Roles;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using GarbageCan.Application.Roles.Commands.ApplyConiditionalRoles;
+
+namespace GarbageCan.Application.UnitTests.Roles.Commands
+{
+    public class ApplyConditionalRoleCommandTests
+    {
+        private ApplicationFixture _appFixture;
+        private IApplicationDbContext _dbContext;
+        private IDiscordGuildRoleService _roleService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _appFixture = new ApplicationFixture();
+            _dbContext = Substitute.For<IApplicationDbContext>();
+            _roleService = Substitute.For<IDiscordGuildRoleService>();
+            _appFixture.OnConfigureServices += (_, services) =>
+            {
+                services.AddSingleton(_dbContext);
+                services.AddSingleton(_roleService);
+            };
+        }
+
+        [Test]
+        public async Task ShouldGrantResultingRoleToMember_WhenMemberHasRequiredRole()
+        {
+            ulong requiredRoleId = 5;
+            ulong resultingRoleId = 553;
+            
+            _dbContext.ConfigureMockDbSet(x => x.ConditionalRoles, new ConditionalRole
+            {
+                id = 0,
+                remain = false,
+                requiredRoleId = requiredRoleId,
+                resultRoleId = resultingRoleId
+            });
+
+            await _appFixture.SendAsync(new ApplyConditionalRolesCommand
+            {
+                GuildId = 1,
+                Members = new Dictionary<ulong, ulong[]>
+                {
+                    {69, new []{requiredRoleId}}
+                }
+            });
+
+            await _roleService.Received().GrantRoleAsync(1, resultingRoleId, 69, Arg.Any<string>());
+        }
+        
+        [Test]
+        public async Task ShouldRevokeResultingRoleFromMember_WhenMemberNoLongerHasRequiredRole()
+        {
+            ulong requiredRoleId = 5;
+            ulong resultingRoleId = 553;
+            ulong userId = 69;
+
+            _dbContext.ConfigureMockDbSet(x => x.ConditionalRoles, new ConditionalRole
+            {
+                id = 0,
+                remain = false,
+                requiredRoleId = requiredRoleId,
+                resultRoleId = resultingRoleId
+            });
+
+            await _appFixture.SendAsync(new ApplyConditionalRolesCommand
+            {
+                GuildId = 1,
+                Members = new Dictionary<ulong, ulong[]>
+                {
+                    {userId, new []{resultingRoleId}}
+                }
+            });
+
+            await _roleService.Received().RevokeRoleAsync(1, resultingRoleId, userId, Arg.Any<string>());
+        }
+        
+        [Test]
+        public async Task ShouldDoNothing_WhenMemberAlreadySatisfiesConditions()
+        {
+            ulong requiredRoleId = 5;
+            ulong resultingRoleId = 553;
+            ulong userId = 69;
+
+            _dbContext.ConfigureMockDbSet(x => x.ConditionalRoles, new ConditionalRole
+            {
+                id = 0,
+                remain = false,
+                requiredRoleId = requiredRoleId,
+                resultRoleId = resultingRoleId
+            });
+
+            await _appFixture.SendAsync(new ApplyConditionalRolesCommand
+            {
+                GuildId = 1,
+                Members = new Dictionary<ulong, ulong[]>
+                {
+                    {userId, new []{requiredRoleId, resultingRoleId}}
+                }
+            });
+
+            await _roleService.DidNotReceiveWithAnyArgs().RevokeRoleAsync(default, default, default);
+            await _roleService.DidNotReceiveWithAnyArgs().GrantRoleAsync(default, default, default);
+        }
+        
+        [Test]
+        public async Task ShouldDoNothing_WhenNoConditionalRolesExist()
+        {
+            ulong requiredRoleId = 5;
+            ulong resultingRoleId = 553;
+            ulong userId = 69;
+
+
+            _dbContext.ConfigureMockDbSet(x => x.ConditionalRoles);
+            
+            await _appFixture.SendAsync(new ApplyConditionalRolesCommand
+            {
+                GuildId = 1,
+                Members = new Dictionary<ulong, ulong[]>
+                {
+                    {userId, new []{requiredRoleId, resultingRoleId}}
+                }
+            });
+
+            await _roleService.DidNotReceiveWithAnyArgs().RevokeRoleAsync(default, default, default);
+            await _roleService.DidNotReceiveWithAnyArgs().GrantRoleAsync(default, default, default);
+        }
+    }
+}
---
+++ GarbageCan.Application/Roles/Commands/ApplyConiditionalRoles/ApplyConditionalRolesCommand.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using GarbageCan.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarbageCan.Application.Roles.Commands.ApplyConiditionalRoles
+{
+    public class ApplyConditionalRolesCommand : IRequest
+    {
+        public ulong GuildId { get; set; }
+        public Dictionary<ulong, ulong[]> Members { get; set; }
+    }
+
+    public class ApplyConditionalRolesCommandHandler : IRequestHandler<ApplyConditionalRolesCommand>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IDiscordGuildRoleService _roleService;
+
+        public ApplyConditionalRolesCommandHandler(IApplicationDbContext context, IDiscordGuildRoleService roleService)
+        {
+            _context = context;
+            this._roleService = roleService;
+        }
+
+        public async Task<Unit> Handle(ApplyConditionalRolesCommand request, CancellationToken cancellationToken)
+        {
+            var conditionalRolesEntity = await _context.ConditionalRoles.ToArrayAsync(cancellationToken);
+            var conditionalRoles = conditionalRolesEntity
+                .GroupBy(r => r.resultRoleId)
+                .ToDictionary(r => r.Key, r => r.Select(x => x.requiredRoleId).ToList());
+
+            var keySet = conditionalRoles.Keys;
+
+            var taskList = new List<Task>();
+
+            foreach (var member in request.Members)
+            {
+                foreach (var role in member.Value.Intersect(keySet)
+                    .Where(r => !member.Value.Intersect(conditionalRoles[r]).Any()))
+                {
+                    // if (conditionalRolesEntity.First(r => r.resultRoleId == role.Id).remain) continue;
+                    await _roleService.RevokeRoleAsync(request.GuildId, role, member.Key, "conditional roles");
+                }
+
+                foreach (var (resultingRole, _) in conditionalRoles
+                    .Where(r => !member.Value.Contains(r.Key))
+                    .Where(r => member.Value.Intersect(r.Value).Any()))
+                {
+                    await _roleService.GrantRoleAsync(request.GuildId, resultingRole, member.Key, "conditional roles");
+                }
+            }
+
+            await Task.WhenAll(taskList);
+
+            return await Task.FromResult(Unit.Value);
+        }
+    }
+}
