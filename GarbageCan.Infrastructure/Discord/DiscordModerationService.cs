c8bc62f3fa5a3cf5bfbabfb4082974bb985f6795
Tim Grannen <grannendev@gmail.com>
2021-06-02 12:41:47 -0400

    Fix moderation service private method name that I screwed up

--- GarbageCan.Infrastructure/Discord/DiscordModerationService.cs
+++ GarbageCan.Infrastructure/Discord/DiscordModerationService.cs
@@ -17,13 +17,13 @@ namespace GarbageCan.Infrastructure.Discord
 
         public async Task BanAsync(ulong guildId, ulong userId, string reason = null)
         {
-            var member = await GetRole(guildId, userId);
+            var member = await GetMember(guildId, userId);
             await member.BanAsync(reason: reason);
         }
 
         public async Task RestoreChannelAccess(ulong guildId, ulong userId, ulong channelId, string reason = null)
         {
-            var member = await GetRole(guildId, userId);
+            var member = await GetMember(guildId, userId);
             var channel = await _client.GetChannelAsync(channelId);
 
             var permissionOverwrites = channel.PermissionOverwrites.FirstOrDefault(o => o.Id == member.Id);
@@ -35,12 +35,12 @@ namespace GarbageCan.Infrastructure.Discord
 
         public async Task RestrictChannelAccess(ulong guildId, ulong userId, ulong channelId)
         {
-            var member = await GetRole(guildId, userId);
+            var member = await GetMember(guildId, userId);
             var channel = await _client.GetChannelAsync(channelId);
             await channel.AddOverwriteAsync(member, Permissions.None, Permissions.AccessChannels);
         }
 
-        private async Task<DiscordMember> GetRole(ulong guildId, ulong userId)
+        private async Task<DiscordMember> GetMember(ulong guildId, ulong userId)
         {
             var guild = await _client.GetGuildAsync(guildId);
             var member = await guild.GetMemberAsync(userId);
