adaed1b2308028f11d7d27a881ad651d190576b3
Tim Grannen <grannendev@gmail.com>
2021-07-17 21:05:42 -0400

    Add unit tests for guild member update event handler

--- GarbageCan.Application.UnitTests/Roles/EventHandlers/DiscordGuildMemberUpdatedHandlerTests.cs
+++ GarbageCan.Application.UnitTests/Roles/EventHandlers/DiscordGuildMemberUpdatedHandlerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
 using GarbageCan.Application.Common.Interfaces;
@@ -16,7 +17,7 @@ namespace GarbageCan.Application.UnitTests.Roles.EventHandlers
     {
         private ApplicationFixture _appFixture;
         private IApplicationDbContext _dbContext;
-        private IDiscordGuildRoleService _discordGuildRoleService;
+        private IDiscordGuildRoleService _roleService;
 
         [SetUp]
         public void Setup()
@@ -24,14 +25,13 @@ namespace GarbageCan.Application.UnitTests.Roles.EventHandlers
             _appFixture = new ApplicationFixture();
             _dbContext = Substitute.For<IApplicationDbContext>();
             _dbContext = Substitute.For<IApplicationDbContext>();
-            _discordGuildRoleService = Substitute.For<IDiscordGuildRoleService>();
+            _roleService = Substitute.For<IDiscordGuildRoleService>();
             _appFixture.OnConfigureServices += (_, services) =>
             {
                 services.AddSingleton(_dbContext);
-                services.AddSingleton(_discordGuildRoleService);
+                services.AddSingleton(_roleService);
             };
         }
-
 
         [Test]
         public async Task ShouldLogError_WhenAnExceptionIsThrown()
@@ -48,6 +48,64 @@ namespace GarbageCan.Application.UnitTests.Roles.EventHandlers
 
             var logger = _appFixture.GetLogger<DiscordGuildMemberUpdatedHandler>();
             logger.ReceivedWithAnyArgs(1).Log(default, default);
+        }
+
+        [Test]
+        public async Task ShouldDoNothing_WhenUserIsNotInWatchList()
+        {
+            ulong userId = 90;
+            ulong otherUserId = 95;
+            _dbContext.ConfigureMockDbSet(x => x.JoinWatchlist, new WatchedUser { id = otherUserId });
+
+            await _appFixture.Publish(new DiscordGuildMemberUpdated
+            {
+                IsBot = false,
+                UserId = userId
+            });
+
+            await _dbContext.DidNotReceiveWithAnyArgs().SaveChangesAsync(default);
+            await _roleService.DidNotReceiveWithAnyArgs().GrantRoleAsync(default, default, default);
+        }
+
+        [Test]
+        public async Task ShouldRemoveFromWatchList_WhenUserIsInWatchList()
+        {
+            ulong userId = 90;
+            var user = new WatchedUser { id = userId };
+            _dbContext.ConfigureMockDbSet(x => x.JoinWatchlist, user);
+
+            await _appFixture.Publish(new DiscordGuildMemberUpdated
+            {
+                IsBot = false,
+                UserId = userId
+            });
+
+            _dbContext.JoinWatchlist.Received(1).Remove(user);
+            await _dbContext.ReceivedWithAnyArgs(1).SaveChangesAsync(default);
+        }
+
+        [Test]
+        public async Task ShouldGrantJoinRole_WhenUserIsInWatchList()
+        {
+            ulong userId = 90;
+            ulong guildId = 51;
+            var user = new WatchedUser { id = userId };
+            _dbContext.ConfigureMockDbSet(x => x.JoinWatchlist, user);
+
+            var roles = new List<JoinRole> { new() { roleId = 9 }, new() { roleId = 942 } };
+            _dbContext.ConfigureMockDbSet(x => x.JoinRoles, roles);
+
+            await _appFixture.Publish(new DiscordGuildMemberUpdated
+            {
+                IsBot = false,
+                UserId = userId,
+                GuildId = guildId
+            });
+
+            foreach (var joinRole in roles)
+            {
+                await _roleService.Received(1).GrantRoleAsync(guildId, joinRole.roleId, userId, "join role");
+            }
         }
     }
 }
