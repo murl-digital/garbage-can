0b431943a0c7379bcfc9ee379899892bacffa5be
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-07-24 18:09:15 -0600

    added auth controller v1

---
+++ GarbageCan.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+
+namespace GarbageCan.Web.Controllers
+{
+    [ApiController]
+    public class AuthenticationController : Controller
+    {
+        [HttpPost("~/connect/authorize")]
+        [HttpGet("~/connect/authorize")]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> Authorize()
+        {
+            var request = HttpContext.GetOpenIddictServerRequest() ??
+                          throw new InvalidOperationException("OpenID Connect request cannot be retrieved");
+
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (!result.Succeeded)
+            {
+                return Challenge("Discord");
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(OpenIddictConstants.Claims.Subject, User.FindFirstValue(ClaimTypes.NameIdentifier))
+            };
+
+            var identity = new ClaimsIdentity(claims, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            principal.SetScopes(request.GetScopes());
+
+            return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+
+        [HttpPost("~/connect/token")]
+        public async Task<IActionResult> Exchange()
+        {
+            var request = HttpContext.GetOpenIddictServerRequest() ??
+                          throw new InvalidOperationException("OpenID Connect request cannot be retrieved");
+
+            ClaimsPrincipal principal;
+            
+            if (request.IsAuthorizationCodeGrantType())
+            {
+                principal = (await HttpContext.AuthenticateAsync(
+                    OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)).Principal;
+            }
+            else
+            {
+                throw new InvalidOperationException("Unsupported grant type");
+            }
+
+            return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+    }
+}
