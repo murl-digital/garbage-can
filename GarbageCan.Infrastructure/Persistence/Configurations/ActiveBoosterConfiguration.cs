15785a23a32989801e9738f1794436488adaaaf3
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-08-02 21:49:48 -0600

    updated/refactored active booster entity

--- GarbageCan.Infrastructure/Persistence/Configurations/ActiveBoosterConfiguration.cs
+++ GarbageCan.Infrastructure/Persistence/Configurations/ActiveBoosterConfiguration.cs
@@ -11,9 +11,13 @@ namespace GarbageCan.Infrastructure.Persistence.Configurations
             // Creates a shadow property for the foriegn key relationship
             // https://docs.microsoft.com/en-us/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key#shadow-foreign-key
             builder.Property<int>("id");
-            builder.HasOne(x => x.slot).WithMany().HasForeignKey("id");
+            builder.HasOne(x => x.Slot).WithMany().HasForeignKey("id");
 
-            builder.Property(x => x.expirationDate).HasColumnType("datetime");
+            builder.Property(x => x.GuildId).HasColumnName("guildId");
+            builder.Property(x => x.ExpirationDate)
+                .HasColumnName("expirationDate")
+                .HasColumnType("datetime");
+            builder.Property(x => x.Multiplier).HasColumnName("multiplier");
             builder.ToTable("xpActiveBoosters");
         }
     }
--- GarbageCan.Infrastructure/Persistence/Migrations/20210803034919_ActiveBoosterRefactor.Designer.cs
+++ GarbageCan.Infrastructure/Persistence/Migrations/20210803034919_ActiveBoosterRefactor.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GarbageCan.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GarbageCan.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210803034919_ActiveBoosterRefactor")]
+    partial class ActiveBoosterRefactor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
