79276e2085594547229295be2ce0cde0b1a486b2
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-08-04 16:40:22 -0600

    commandmediator modification

--- GarbageCan.Web/Commands/CommandMediator.cs
+++ GarbageCan.Web/Commands/CommandMediator.cs
@@ -1,11 +1,11 @@
-﻿using DSharpPlus.CommandsNext;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
 using GarbageCan.Infrastructure.Discord;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace GarbageCan.Web.Commands
 {
@@ -20,7 +20,7 @@ namespace GarbageCan.Web.Commands
             _logger = logger;
         }
 
-        public async Task Send<TResponse>(IRequest<TResponse> request,
+        public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request,
             CommandContext commandContext,
             CancellationToken cancellationToken = new CancellationToken())
         {
@@ -33,12 +33,13 @@ namespace GarbageCan.Web.Commands
                 var contextService = scope.ServiceProvider.GetRequiredService<DiscordCommandContextService>();
                 contextService.CommandContext = commandContext;
 
-                await mediator.Send(request, cancellationToken);
+                return await mediator.Send(request, cancellationToken);
             }
             catch (Exception exception)
             {
                 _logger.LogError(exception, "An error occurred during a command. Request Type: {Request}", request.GetType().Name);
                 await commandContext.RespondAsync("An error occurred");
+                return default;
             }
         }
     }
