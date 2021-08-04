7c8a80e62921147f3d700a5eb5ed2cdfccbfeb68
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-08-04 14:38:59 -0600

    add userbooster commands and queries

---
+++ GarbageCan.Application/Boosters/UserBoosters/Queries/GetUserBoosterQuery.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using GarbageCan.Domain.Entities.Boosters;
+using MediatR;
+
+namespace GarbageCan.Application.Boosters.Queries
+{
+    public class GetUserBoosterQuery : IRequest<UserBooster>
+    {
+        public ulong GuildId { get; set; }
+        public ulong UserId { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class GetUserBoosterQueryHandler : IRequestHandler<GetUserBoosterQuery, UserBooster>
+    {
+        private readonly IMediator _mediator;
+
+        public GetUserBoosterQueryHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<UserBooster> Handle(GetUserBoosterQuery request, CancellationToken cancellationToken)
+        {
+            var boosters = await _mediator.Send(new GetUserBoostersQuery
+            {
+                GuildId = request.GuildId,
+                UserId = request.UserId
+            });
+
+            return boosters.FirstOrDefault(b => b.Id == request.Id);
+        }
+    }
+}
