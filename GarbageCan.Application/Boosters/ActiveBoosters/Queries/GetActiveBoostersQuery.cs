47ac3c04e2eee8e98dec0bda917c6884e72e9f72
Joe Sorensen <joseph.md.sorensen@gmail.com>
2021-08-04 15:46:13 -0600

    added GetActiveBoostersQuery

---
+++ GarbageCan.Application/Boosters/ActiveBoosters/Queries/GetActiveBoostersQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using GarbageCan.Application.Common.Interfaces;
+using GarbageCan.Domain.Entities.Boosters;
+using MediatR;
+
+namespace GarbageCan.Application.Boosters.ActiveBoosters.Queries
+{
+    public class GetActiveBoostersQuery : IRequest<ActiveBooster[]>
+    {
+        public ulong GuildId { get; set; }
+    }
+
+    public class GetActiveBoostersQueryHandler : RequestHandler<GetActiveBoostersQuery, ActiveBooster[]>
+    {
+        private readonly IBoosterService _boosterService;
+
+        public GetActiveBoostersQueryHandler(IBoosterService boosterService)
+        {
+            _boosterService = boosterService;
+        }
+
+        protected override ActiveBooster[] Handle(GetActiveBoostersQuery request)
+        {
+            return _boosterService.ActiveBoosters.TryGetValue(request.GuildId, out var boosters)
+                ? boosters.ToArray()
+                : Array.Empty<ActiveBooster>();
+        }
+    }
+}
