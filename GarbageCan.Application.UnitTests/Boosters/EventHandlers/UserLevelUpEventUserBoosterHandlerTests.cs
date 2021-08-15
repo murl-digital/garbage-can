f824141958711342f5832d9b27076dbb4c7fe129
Tim Grannen <grannendev@gmail.com>
2021-08-15 14:33:10 -0400

    Add User level up event handler to create user boosters

---
+++ GarbageCan.Application.UnitTests/Boosters/EventHandlers/UserLevelUpEventUserBoosterHandlerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GarbageCan.Application.Boosters.EventHandlers;
+using GarbageCan.Application.Boosters.UserBoosters.Commands;
+using GarbageCan.Application.Common.Models;
+using GarbageCan.Domain.Events;
+using MediatR;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace GarbageCan.Application.UnitTests.Boosters.EventHandlers
+{
+    public class UserLevelUpEventUserBoosterHandlerTests
+    {
+        private IMediator _mediator;
+        private UserLevelUpEventUserBoosterHandler _sut;
+        private const ulong GuildId = 50;
+        private const ulong UserId = 5;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mediator = Substitute.For<IMediator>();
+
+            _sut = new UserLevelUpEventUserBoosterHandler(_mediator);
+        }
+
+        [Test]
+        [TestCase(0, 1)]
+        [TestCase(4, 5)]
+        [TestCase(3, 9)]
+        public async Task Handle_ShouldNotCreateUserBooster_WhenLevelGreaterThan10(int oldLevel, int newLevel)
+        {
+            await RunHandler(oldLevel, newLevel);
+
+            await _mediator.DidNotReceiveWithAnyArgs().Send(Arg.Any<AddUserBoosterCommand>(), Arg.Any<CancellationToken>());
+        }
+
+        [Test]
+        [TestCase(10, 11)]
+        [TestCase(10, 14)]
+        [TestCase(11, 14)]
+        public async Task Handle_ShouldNotCreateUserBooster_WhenLevelIsNotAMultipleOf5(int oldLevel, int newLevel)
+        {
+            await RunHandler(oldLevel, newLevel);
+            await _mediator.DidNotReceiveWithAnyArgs().Send(Arg.Any<AddUserBoosterCommand>(), Arg.Any<CancellationToken>());
+        }
+
+        [Test]
+        [TestCase(9, 10)]
+        [TestCase(14, 15)]
+        [TestCase(11, 19)]
+        public async Task Handle_ShouldCreateUserBooster_WhenLevelIsAMultipleOf5OrHasCrossedAMultipleOf5(int oldLevel, int newLevel)
+        {
+            await RunHandler(oldLevel, newLevel);
+
+            await _mediator.Received(1).Send(
+                Arg.Is<AddUserBoosterCommand>(command =>
+                    command.GuildId == GuildId &&
+                    command.UserId == UserId &&
+                    command.Duration == TimeSpan.FromMinutes(30) &&
+                    Math.Abs(command.Multiplier - 1.5f) < 0.0001
+                ),
+                Arg.Any<CancellationToken>());
+        }
+
+        private async Task RunHandler(int oldLevel, int newLevel)
+        {
+            await _sut.Handle(new DomainEventNotification<UserLevelUpEvent>(new UserLevelUpEvent
+            {
+                GuildId = GuildId,
+                UserId = UserId,
+                OldLvl = oldLevel,
+                NewLvl = newLevel,
+            }), CancellationToken.None);
+        }
+    }
+}
---
+++ GarbageCan.Application/Boosters/EventHandlers/UserLevelUpEventUserBoosterHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using GarbageCan.Application.Boosters.UserBoosters.Commands;
+using GarbageCan.Application.Common.Models;
+using GarbageCan.Domain.Events;
+using MediatR;
+
+namespace GarbageCan.Application.Boosters.EventHandlers
+{
+    public class UserLevelUpEventUserBoosterHandler : INotificationHandler<DomainEventNotification<UserLevelUpEvent>>
+    {
+        private readonly IMediator _mediator;
+
+        public UserLevelUpEventUserBoosterHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        public async Task Handle(DomainEventNotification<UserLevelUpEvent> notification, CancellationToken cancellationToken)
+        {
+            var oldLevel = notification.DomainEvent.OldLvl;
+            var newLevel = notification.DomainEvent.NewLvl;
+
+            var shouldCreate =
+                Enumerable.Range(oldLevel + 1, Math.Abs(newLevel - oldLevel))
+                    .Any(x => x >= 10 && x % 5 == 0);
+            if (!shouldCreate)
+            {
+                return;
+            }
+
+            await _mediator.Send(new AddUserBoosterCommand
+            {
+                GuildId = notification.DomainEvent.GuildId,
+                UserId = notification.DomainEvent.UserId,
+                Multiplier = 1.5f,
+                Duration = TimeSpan.FromMinutes(30)
+            }, cancellationToken);
+        }
+    }
+}
