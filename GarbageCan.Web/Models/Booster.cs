358d75d596866417bacbbc86da5f2077a62fbb41
Tim Grannen <grannendev@gmail.com>
2021-09-06 00:00:39 -0400

    Remove old XP comment

--- GarbageCan.Web/Models/Booster.cs
+++ GarbageCan.Web/Models/Booster.cs
@@ -5,7 +5,7 @@ namespace GarbageCan.Web.Models
 {
     public class Booster
     {
-        [SwaggerSchema("The XP multiplier. Internally 1.0 is added to the total (e.g. 0.5 -> 1.5)")]
+        [SwaggerSchema("The XP multiplier")]
         public float Multiplier { get; set; }
         [SwaggerSchema("The date and time this booster will expire at, in UTC")]
         public DateTime Expiration { get; set; }
--- GarbageCan.Web/Models/QueuedBooster.cs
+++ GarbageCan.Web/Models/QueuedBooster.cs
@@ -4,7 +4,7 @@ namespace GarbageCan.Web.Models
 {
     public class QueuedBooster
     {
-        [SwaggerSchema("The XP multiplier. Internally 1.0 is added to the total (e.g. 0.5 -> 1.5)")]
+        [SwaggerSchema("The XP multiplier")]
         public float Multiplier { get; set; }
         [SwaggerSchema("This booster's duration, in seconds. This is converted into a UTC timestamp when activated")]
         public long DurationInSeconds { get; set; }
