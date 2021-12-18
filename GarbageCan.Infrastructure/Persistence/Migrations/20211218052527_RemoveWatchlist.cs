29855ea2888973a57bf0c96547137d32acbd5865
Joe Sorensen <hello@murl.digital>
2021-12-17 22:36:17 -0700

    remove watchlist from db

---
+++ GarbageCan.Infrastructure/Persistence/Migrations/20211218052527_RemoveWatchlist.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GarbageCan.Infrastructure.Persistence.Migrations
+{
+    public partial class RemoveWatchlist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "joinWatchlist");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "joinWatchlist",
+                columns: table => new
+                {
+                    guildId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    userId = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                },
+                constraints: table =>
+                {
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_joinWatchlist_guildId",
+                table: "joinWatchlist",
+                column: "guildId");
+        }
+    }
+}
