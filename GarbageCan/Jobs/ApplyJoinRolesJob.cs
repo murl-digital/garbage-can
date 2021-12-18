77f05f93033d4eab788ba7c646d75c000a0abb77
Joe Sorensen <hello@murl.digital>
2021-12-17 22:32:58 -0700

    added ApplyJoinRolesJob

---
+++ GarbageCan/Jobs/ApplyJoinRolesJob.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using GarbageCan.Application.Roles.JoinRoles.Commands.ApplyJoinRoles;
+using MediatR;
+using Quartz;
+
+namespace GarbageCan.Jobs
+{
+    public class ApplyJoinRolesJob : IJob
+    {
+        private readonly IMediator _mediator;
+
+        public ApplyJoinRolesJob(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await _mediator.Send(new ApplyJoinRolesCommand());
+        }
+    }
+}
